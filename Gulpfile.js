dc7fa74d22d35e04bce65f9763336f45f8cc4fe8
Sebastian Palacio <sebastianpalacio327@gmail.com>
2016-05-27 01:12:47 -0500

    gulp commnads for browserify and babel

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require ('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('styles', function(){
 	gulp
@@ -10,4 +13,20 @@ gulp.task('styles', function(){
 	.pipe(gulp.dest('public'));
 })
 
-gulp.task('default',['styles'])
+gulp.task('assets', function (){
+	gulp
+	.src('assets/*')
+	.pipe(gulp.dest('public'));
+	//glob - regular expression
+})
+
+gulp.task('scripts', function(){
+	browserify('./src/index.js')
+	.transform(babel)
+	.bundle()
+	.pipe(source('index.js'))
+	.pipe(rename('app.js'))
+	.pipe(gulp.dest('public'));
+})
+
+gulp.task('default',['styles', 'assets','scripts'])
