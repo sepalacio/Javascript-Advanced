c372cc82ab506c111493104b139fc862878ab7c9
Sebastian Palacio <sebastianpalacio327@gmail.com>
2016-06-01 00:36:42 -0500

    CLint side Javascript

--- public/app.js
+++ public/app.js
@@ -1,4 +1,17 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-alert("hola");
+var numeros = [400, 200, 1, -23];
+
+//function that executes for each element of the array
+
+//with vanilla JS
+// var numerosPlus1 = numeros.map(function(numero){
+// 	return numero + 1;
+// })
+
+//Ecma script 6
+
+var numerosPlus1 = numeros.map(n => n + 1);
+
+console.log(numerosPlus1);
 
 },{}]},{},[1]);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// alert("hola");
+var numeros = [400, 200,1,-23];
+
+
+//function that executes for each element of the array
+
+//with vanilla JS
+// var numerosPlus1 = numeros.map(function(numero){
+// 	return numero + 1;
+// })
+
+//Ecma script 5
+
+var numerosPlus1 = numeros.map(n => n+1);
+
+
+console.log(numerosPlus1);
+
