27f17b10683d9470d616bbdf2fec33b41fb3f9c8
Sebastian Palacio <sebastianpalacio327@gmail.com>
2016-07-22 00:34:04 -0500

    Http request with Superagent library

--- server.js
+++ server.js
@@ -25,6 +25,34 @@ app.get('/signin', function (req, res){
 	});
 })
 
+app.get('/api/pictures', function(req, res, next){
+	var pictures = [
+		{
+			user: {
+				'username': 'sebas',
+				'avatar':'http://zblogged.com/wp-content/uploads/2015/11/17.jpg'				
+			},
+			url:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwD_ZOkV3ALOJBAsKTlz82nyU2tBh7r-1YgsL1K2PdobJpeUgbuQ',
+			likes: 0,
+			liked: false,
+			createdAt: new Date().getTime()
+		},
+		{
+			user: {
+				'username': 'sebas',
+				'avatar':'http://zblogged.com/wp-content/uploads/2015/11/17.jpg'				
+			},
+			url:'office.jpg',
+			likes: 1,
+			liked: true,
+			createdAt: new Date().setDate(new Date().getDate()-10)
+		}
+	];
+	setTimeout(function(){
+		res.send(pictures);
+	}, 2000);
+})
+
 app.listen(3000, function(err){
 	if (err) return console.log("There is a mistake"), process.exit(1);
 
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,30 +3,22 @@ var empty = require('empty-element');
 var main = document.getElementById('main-container');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header =require('../header');
 
-page('/' , function (ctx, next){
+page('/' ,header,loadPictures, function (ctx, next){
 	title('Platzigram');
-	var pictures = [
-		{
-			user: {
-				'username': 'sebas',
-				'avatar':'http://zblogged.com/wp-content/uploads/2015/11/17.jpg'				
-			},
-			url:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwD_ZOkV3ALOJBAsKTlz82nyU2tBh7r-1YgsL1K2PdobJpeUgbuQ',
-			likes: 0,
-			liked: false,
-			createdAt: new Date()
-		},
-		{
-			user: {
-				'username': 'sebas',
-				'avatar':'http://zblogged.com/wp-content/uploads/2015/11/17.jpg'				
-			},
-			url:'office.jpg',
-			likes: 1,
-			liked: true,
-			createdAt: new Date().setDate(new Date().getDate()-10)
-		}
-	];
-    empty(main).appendChild(template(pictures));
+
+
+    empty(main).appendChild(template(ctx.pictures));
 })
+
+function loadPictures(ctx, next){
+	request
+		.get('/api/pictures')
+		.end( function(err, res){
+			if(err) return console.log(err);
+			ctx.pictures = res.body;
+			next();
+		})
+}
