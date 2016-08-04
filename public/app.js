f169e11d47b74c71296d1601252fbe81edac3beb
Sebastian Palacio <sebastianpalacio327@gmail.com>
2016-08-04 01:18:23 -0500

    Upload image to the server with multer library

--- public/app.js
+++ public/app.js
@@ -15723,54 +15723,59 @@ var yo = require('yo-yo');
 var layout = require('../layout');
 var picture = require('../picture-card');
 var translate = require('../translate').message;
+var request = require('superagent');
 
 module.exports = function (pictures) {
-	var content = yo`<div class="container timeline">
-	<div class="row">
-	<div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
-		<form enctype="multipart/form-data" id="formUpload" class="form-upload">
-			<div id="fileName" class="fileUpload btn btn-flat cyan">
-				<span>
-					<i class="fa fa-camera"></i>
-				${ translate('upload-picture') }</span>
-				<input type="file" name="picture" id="file" class="upload" onchange=${ onchange } />
-				<button type="submit" id="btnUpload" class="btn btn-flat cyan hide">
-					${ translate('upload') }
-				</button>
-				<button type="button" id="btnCancel" onclick=${ cancel } class="btn btn-flat red hide">
-					<i class="fa fa-cancel"></i>
-				</button>
-			</div>
-		</form>
-	</div>
-	</div>
-		<div class="row">
-			<div class="col s12 m10 offset-m1 l6 offset-l3">
-				${ pictures.map(function (pic) {
-		return picture(pic);
-	}) }
-			</div>
-		</div>
-	</div>`;
-	function onchange() {
-		toggleButtons();
-	}
+  var el = yo`<div class="container timeline">
+    <div class="row">
+      <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
+        <form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${ onsubmit }>
+          <div id="fileName" class="fileUpload btn btn-flat cyan">
+            <span><i class="fa fa-camera" aria-hidden="true"></i> ${ translate('upload-picture') }</span>
+            <input name="picture" id="file" type="file" class="upload" onchange=${ onchange } />
+          </div>
+          <button id="btnUpload" type="submit" class="btn btn-flat cyan hide">${ translate('upload') }</button>
+          <button id="btnCancel" type="button" class="btn btn-flat red hide" onclick=${ cancel }><i class="fa fa-times" aria-hidden="true"></i></button>
+        </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12 m10 offset-m1 l6 offset-l3">
+        ${ pictures.map(function (pic) {
+    return picture(pic);
+  }) }
+      </div>
+    </div>
+  </div>`;
 
-	function cancel() {
-		toggleButtons();
-		document.getElementById('formUpload').reset();
-	}
+  function toggleButtons() {
+    document.getElementById('fileName').classList.toggle('hide');
+    document.getElementById('btnUpload').classList.toggle('hide');
+    document.getElementById('btnCancel').classList.toggle('hide');
+  }
 
-	function toggleButtons() {
-		document.getElementById('fileName').classList.toggle('hide');
-		document.getElementById('btnUpload').classList.toggle('hide');
-		document.getElementById('btnCancel').classList.toggle('hide');
-	}
+  function cancel() {
+    toggleButtons();
+    document.getElementById('formUpload').reset();
+  }
 
-	return layout(content);
+  function onchange() {
+    toggleButtons();
+  }
+
+  function onsubmit(ev) {
+    ev.preventDefault();
+    var data = new FormData(this);
+
+    request.post('/api/pictures').send(data).end(function (err, res) {
+      console.log(arguments);
+    });
+  }
+
+  return layout(el);
 };
 
-},{"../layout":79,"../picture-card":80,"../translate":87,"yo-yo":66}],77:[function(require,module,exports){
+},{"../layout":79,"../picture-card":80,"../translate":87,"superagent":60,"yo-yo":66}],77:[function(require,module,exports){
 //load page dependecy with browserify
 var page = require('page');
 
--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 var express = require('express');
+var multer  = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now()+ '.' + ext(file.originalname))
+  }
+})
+var upload = multer({ storage: storage }).single('picture');
 var app = express();
 
 app.set('view engine', 'pug');
@@ -51,6 +63,15 @@ app.get('/api/pictures', function(req, res, next){
 	setTimeout(function(){
 		res.send(pictures);
 	}, 2000);
+})
+
+app.post('/api/pictures', function(req, res){
+	upload(req, res, function(err){
+		if(err){
+			return res.send(500, "Error uploading file");
+		}
+		res.send("The file was uploaded succesfully");
+	})
 })
 
 app.listen(3000, function(err){
